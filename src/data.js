2f35db894c480c0048a9a3bad828560935c43453
edahmitchel <edahmitchel@gamil.com>
2022-05-07 23:46:09 +0100

    data

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+        "name":"Marc-André Giroux",
+        "title":"GraphQL is a Trap?",
+        "date":"May 6",
+        "length":"6"
+    },
+    {
+        "name":"Chris Nielsen",
+        "title":"i replaced my native iOS app with a cross platform web app and no-one noticed",
+        "date":"may 3",
+        "length":"7"
+    },
+    {
+        "name":"Eryk Lewinson",
+        "title":"PyScript—unleash the power of Python in your browser",
+        "date":"may 2",
+        "length":"5"
+    },
+    {
+        "name":"Sebastian Carlos",
+        "title":"Austria buys JavaScript from Brendan Eich",
+        "date":"April 30",
+        "length":"3"
+    },
+    {
+        "name":"Sean Kernan",
+        "title":"Johnny Depp Willp Will Likely Lose His $50M Defamation Case",
+        "date":"April 29",
+        "length":5
+    },
+    {
+        "name":"Barack Obama",
+        "title":"My Statement with Michelle on the Draft Supreme Court Decision to Overturn Roe v. Wade",
+        "date":" May 3",
+        "length":"3"
+    }
+]
